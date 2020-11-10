79aa3cd9cc20ac310f4954cd50d6bdcda57153d3
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-09 23:18:44 -0500

    finished the index.js

---
+++ Develop/index.js
@@ -0,0 +1,115 @@
+
+// Requires nodes file system commands
+const fs = require('fs')
+
+//requires npm inquirer
+const inquirer = require('inquirer')
+
+//requires the generate markdown functions which are exported from generateMarkdown.js
+const create = require('./utils/generateMarkdown.js')
+
+
+const questions = [{
+    type: "input",
+    name: "title",
+    message: "Please create a title for your program"
+},
+{
+    type: "input",
+    name: "description",
+    message: "Please describe your program in detail"
+},
+{
+    type: "input",
+    name: "installation",
+    message: "Please describe the steps needed to install your program"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "How would someone use this program?"
+},
+{
+    type: "input",
+    name: "license",
+    message: "What licensing may your program require?",
+},
+{
+    type: "input",
+    name: "contributors",
+    message: "Who was involved in the creation of this program?"
+},
+
+{
+    type: "input",
+    name: "username",
+    message: "What username can we use to find you on Github?"
+},
+
+{
+    type: "input",
+    name: "email",
+    message: "Please provide your email address"
+},
+
+{
+    type: "checkbox",
+    name: "badges",
+    message: "What badges would you like to include in your Readme?",
+    choices: [
+        'JavaScript',
+        'HTML',
+        'CSS'
+    ]
+}
+];
+
+async function writeToFile() {
+    const userInput = await inquirer.prompt(questions, (err) => console.log(err))
+
+
+    let badgeType = userInput.badges
+
+    
+    function addBadges() {
+        if (badgeType == 'JavaScript') {
+            userInput.badges = `![JavaScript](https://img.shields.io/github/languages/top/nielsenjared/badmath)`
+        }
+
+        else if (badgeType == 'HTML') {
+            userInput.badges = `![HTML](https://img.shields.io/badge/HTML-100%25-orange)`
+        }
+
+        else if (badgeType == 'CSS') {
+            userInput.badges = `![CSS](https://img.shields.io/badge/CSS-100%25-yellowgreen)`
+        }
+    }
+    addBadges()
+
+
+    fs.writeFile("YourGeneratedReadMe.md", create(userInput), (err) => console.log(err))
+    console.log("Thank you using the ReadMe Generator. Your read me file is now generated and should be visible in your local folder.")
+}
+
+writeToFile()
+
+
+
+
+
+
+/*
+function AskQuestions()
+
+
+// function to write README file
+function writeToFile(results.json, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init(); */
